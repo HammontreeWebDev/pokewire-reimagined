3e6c2b5907e88ecb301237d954b2519d2c6e8527
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-05 16:43:59 -0400

    added border radius to log in form

--- app/components/login/LoginForm.jsx
+++ app/components/login/LoginForm.jsx
@@ -2,7 +2,7 @@
 export default function LogInForm() {
     return (
         <>
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 antialiased bg-poke-yellow">
+            <div className="antialiased bg-poke-yellow flex min-h-full flex-1 flex-col justify-center px-6 py-12 rounded-2xl lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
                         className="mx-auto h-10 w-auto"
