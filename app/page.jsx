11ffdb8f2c2e7e6a9763452b491b92a81ae14a5c
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-05 15:15:49 -0400

    added login button

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import LogInNav from "@/app/components/navbars/LogInNav";
 
 export default function Home() {
   return (
+    <>
+    <header>
+      <LogInNav />
+    </header>
+
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
       <p>This is the home page</p>
+
     </main>
+    </>
   );
 }
