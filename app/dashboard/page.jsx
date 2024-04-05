5f4cd345f09a280db1889caf7f785fe386b13702
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-05 16:49:43 -0400

    added boilerplate

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,7 @@
+// TODO: I assume the logic for whether this page shows up based on user log in status will be imported somewhere in this file.
+
+export default function Dashboard() {
+    return (
+            <p>This is the dashboard!</p>
+    )
+}
