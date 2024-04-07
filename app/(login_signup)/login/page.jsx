b8491eb372727c78a0e91561bfc67822e5a09fd3
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-07 11:31:55 -0400

    update session provider to wrap the app globally

--- app/(login_signup)/login/page.jsx
+++ app/(login_signup)/login/page.jsx
@@ -1,11 +1,11 @@
-'use client'
+// 'use client'
 import LogInForm from "@/app/ui/login/LoginForm";
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
 
 export default function Login() {
     return (
-        <SessionProvider>
+        // <SessionProvider>
             <LogInForm />
-        </SessionProvider>
+        // </SessionProvider>
     )
 }
