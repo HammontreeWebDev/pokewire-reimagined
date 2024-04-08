10aba67fc39ac27e544136324d6fbd8492c2ee0d
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-08 15:29:35 -0400

    added boiler plate for profile page and moved route

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,10 +1,11 @@
 'use client'
 import userAuthentication from "@/app/utils/userAuthentication";
+import ProfileBase from "@/app/ui/profile/ProfileBase";
 
 export default function Profile() {
     userAuthentication();
 
     return (
-        <p>This is the Profile!</p>
+        <ProfileBase />
     )
 }
