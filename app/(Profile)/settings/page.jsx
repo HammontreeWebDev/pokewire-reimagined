4cc1440e88a0b951d10f934cb39fee310478fe6b
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-08 22:49:06 -0400

    moved some folders around - settings and profile now share same blank layout

---
+++ app/(Profile)/settings/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+import userAuthentication from "@/app/utils/userAuthentication";
+
+
+export default function SettingsPage() {
+    userAuthentication();
+
+    return (
+
+        <>
+
+        </>
+
+    )
+}
