36d3403953f6d539665bb9d1c944e788b5a2caa4
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-08 23:06:03 -0400

    added side nav on settings page - some refactoring done

--- app/(Profile)/settings/page.jsx
+++ app/(Profile)/settings/page.jsx
@@ -1,15 +1,18 @@
 'use client'
 import userAuthentication from "@/app/utils/userAuthentication";
-
+import SideNav from "@/app/ui/settings/SideNav";
 
 export default function SettingsPage() {
     userAuthentication();
 
     return (
 
-        <>
-
-        </>
+        <div className="flex min-h-screen">
+            <SideNav />
+            <main className="flex-1 p-8">
+                <p>this is the settings page</p>
+            </main>
+        </div>
 
     )
 }
--- app/ui/settings/SideNav.jsx
+++ app/ui/settings/SideNav.jsx
@@ -1,6 +1,5 @@
 import {
     CalendarIcon,
-    ChartPieIcon,
     DocumentDuplicateIcon,
     FolderIcon,
     HomeIcon,
@@ -8,17 +7,11 @@ import {
   } from '@heroicons/react/24/outline'
   
   const navigation = [
-    { name: 'Dashboard', href: '#', icon: HomeIcon, count: '5', current: true },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, count: '12', current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, count: '20+', current: false },
-    { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-  ]
-  const teams = [
-    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+    { name: 'Home', href: 'settings/home', icon: HomeIcon, current: false },
+    { name: 'My Pokémon', href: '/settings/pokemon', icon: UsersIcon, current: false },
+    { name: 'Avatar', href: 'gamedex', icon: FolderIcon, current: false },
+    { name: 'Email Address', href: '/settings/email-address', icon: CalendarIcon, current: false },
+    { name: 'Help', href: 'settings/help', icon: DocumentDuplicateIcon, current: false },
   ]
   
   function classNames(...classes) {
@@ -58,27 +51,6 @@ import {
                           {item.count}
                         </span>
                       ) : null}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </li>
-            <li>
-              <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-              <ul role="list" className="-mx-2 mt-2 space-y-1">
-                {teams.map((team) => (
-                  <li key={team.name}>
-                    <a
-                      href={team.href}
-                      className={classNames(
-                        team.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                      )}
-                    >
-                      <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                        {team.initial}
-                      </span>
-                      <span className="truncate">{team.name}</span>
                     </a>
                   </li>
                 ))}
