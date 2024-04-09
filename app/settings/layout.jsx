dc4da738d82ad971ee457ecce9f434e20b41f0f6
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-08 23:54:35 -0400

    updated flex properties

--- app/settings/layout.jsx
+++ app/settings/layout.jsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
     return (
         <div className="flex min-h-screen">
             <SideNav />
-            <main className="flex-2 p-8 min-h-screen bg-gray-500">
+            <main className="flex-1 p-8 min-h-screen bg-gray-500 w-full">
                 {children}
             </main>
         </div>
