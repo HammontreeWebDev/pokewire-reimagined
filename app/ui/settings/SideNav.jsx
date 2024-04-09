df1bc03769ba903eb26f04092c04517d5f271922
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-08 23:16:13 -0400

    updated css classes for colors

--- app/ui/settings/SideNav.jsx
+++ app/ui/settings/SideNav.jsx
@@ -41,20 +41,12 @@ export default function SideNav() {
                   <Link
                     href={item.href}
                     className={classNames(
-                      item.current ? 'bg-gray-800 text-white' : 'text-gray-400 hover:text-white hover:bg-gray-800',
+                      item.current ? 'bg-poke-blue text-poke-yellow' : 'text-poke-white hover:bg-[var(--dark-yellow)] hover:text-[var(--poke-red)]',
                       'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                     )}
                   >
                     <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
                     {item.name}
-                    {item.count ? (
-                      <span
-                        className="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-gray-900 px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-white ring-1 ring-inset ring-gray-700"
-                        aria-hidden="true"
-                      >
-                        {item.count}
-                      </span>
-                    ) : null}
                   </Link>
                 </li>
               ))}
