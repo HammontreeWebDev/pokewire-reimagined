3eba8e2a73b29d62c62f6b02185555863282a424
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-16 12:16:04 -0400

    recommit of last commit

--- app/ui/pokefinder/DataDisplay.jsx
+++ app/ui/pokefinder/DataDisplay.jsx
@@ -76,7 +76,7 @@ export default function DataDisplay() {
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <dt className="text-sm font-medium leading-6 text-white">{`${selectedPokemon}'s Legacy Cry`}</dt>
                             <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">
-                                {/* // TODO: need to make it so that only  */}
+                                {/* // TODO: need to make it so that only the current pokemons legacy cry shows - at the moment it is displaying prev pokemons legacy cry if there was not one found for current pokemon selected */}
                                 <audio src={legacyCry} controls></audio>
                             </dd>
                         </div>
