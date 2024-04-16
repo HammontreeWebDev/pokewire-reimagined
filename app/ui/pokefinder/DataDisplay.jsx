c91db27eac18f48effcd41bc44fc815776be071c
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-16 12:15:28 -0400

    several pieces of data now displayed

--- app/ui/pokefinder/DataDisplay.jsx
+++ app/ui/pokefinder/DataDisplay.jsx
@@ -5,21 +5,45 @@ import { useEffect, useState } from "react";
 export default function DataDisplay() {
 
     const [selectedPokemon] = usePokemon();
+    const [baseExperience, setBaseExperience] = useState('');
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [abilities, setAbilities] = useState([]);
+    const [moves, setMoves] = useState([]);
+    const [latestCry, setLatestCry] = useState('');
+    const [legacyCry, setLegacyCry] = useState('');
 
     const genericURL = `https://pokeapi.co/api/v2/pokemon/${selectedPokemon.toLowerCase()}`;
 
     useEffect(() => {
         if (selectedPokemon.length > 3) {
             fetch(genericURL)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data)
-        })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data)
+                    setLatestCry(data.cries.latest);
+                    setLegacyCry(data.cries.legacy);
+                    setBaseExperience(data.base_experience);
+                    setHeight(data.height);
+                    setWeight(data.weight);
+
+                    setAbilities(data.abilities.map(ability => ({
+                        name: ability.ability.name,
+                        isHidden: ability.is_hidden,
+                        slot: ability.slot
+                    })));
+
+                    setMoves(data.moves.map(move => ({
+                        name: move.move.name,
+                        url: move.url
+                    })));
+                })
+                .catch(error => console.error("Failed to fetch data:", error));
         }
     }, [selectedPokemon])
 
@@ -33,21 +57,74 @@ export default function DataDisplay() {
                 </div>
                 <div className="mt-6 border-t border-white/10">
                     <dl className="divide-y divide-white/10">
+
+                        {/* //! Pokémon Name */}
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <dt className="text-sm font-medium leading-6 text-white">Pokémon</dt>
                             <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">{selectedPokemon}</dd>
                         </div>
+
+                        {/* //! Pokémon Latest cry */}
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-white">Pokémon Stats</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">(live data)</dd>
+                            <dt className="text-sm font-medium leading-6 text-white">{`${selectedPokemon}'s Latest Cry`}</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">
+                                <audio src={latestCry} controls ></audio>
+                            </dd>
                         </div>
+
+                        {/* //! Pokémon Legacy cry */}
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-white">Pokémon Stats</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">(live data)</dd>
+                            <dt className="text-sm font-medium leading-6 text-white">{`${selectedPokemon}'s Legacy Cry`}</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">
+                                {/* // TODO: need to make it so that only  */}
+                                <audio src={legacyCry} controls></audio>
+                            </dd>
                         </div>
+
+                        {/* //! Pokémon Base Experience */}
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                            <dt className="text-sm font-medium leading-6 text-white">Salary expectation</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">(live data)</dd>
+                            <dt className="text-sm font-medium leading-6 text-white">Base Experience</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">{baseExperience}</dd>
+                        </div>
+
+                        {/* //! Pokémon Height */}
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-white">Height</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">{height}</dd>
+                        </div>
+
+                        {/* //! Pokémon Weight */}
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-white">Weight</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">{weight}</dd>
+                        </div>
+
+                        {/* //! Pokémon Abilities */}
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-white">Abilities</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">
+                                {
+                                    abilities.map((ability, index) => (
+                                        <div key={index}>
+                                            {ability.name}
+                                        </div>
+                                    ))
+                                }
+                            </dd>
+                        </div>
+
+                        {/* //! Pokémon Moves */}
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <dt className="text-sm font-medium leading-6 text-white">Moves</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">
+                                {
+                                    moves.map((move, index) => (
+                                        <div key={index}>
+                                            {move.name}
+                                        </div>
+                                    ))
+                                }
+                            </dd>
                         </div>
                     </dl>
                 </div>
