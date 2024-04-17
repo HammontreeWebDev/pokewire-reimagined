3ae493634de56e7f965d7cfc7992245139325094
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-17 14:32:26 -0400

    created custom audio player

---
+++ app/ui/audio/AudioPlayer.jsx
@@ -0,0 +1,73 @@
+import { useState, useRef } from "react";
+import { SpeakerWaveIcon } from "@heroicons/react/24/outline";
+import { PlayIcon, PauseIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+
+export default function AudioPlayer({ imageURL, imageAlt, soundTitle, audioSrc }) {
+
+    const [play, setPlay] = useState(false);
+    const pokeRef = useRef < HTMLAudioElement > (null);
+    const MAX = 20;
+
+    function toggleAudio() {
+        if (play) {
+            pokeRef.current?.pause();
+            setPlay(false);
+        } else {
+            pokeRef.current?.play();
+            setPlay(true);
+        }
+    }
+
+    function handleVolume(e) {
+        const { value } = e.target;
+        const volume = Number(value) / MAX;
+        pokeRef.current.volume = volume;
+    }
+
+    return (
+        <>
+            <main className="flex min-h-screen flex-col items-center justify-center bg-background">
+                <div className="bg-accent flex h-fit max-w-fit flex-col rounded-lg border-2 border-cyan-700 pb-4 text-center shadow">
+                    <div className="relative flex flex-col space-y-0">
+                        <Image
+                            width={200}
+                            height={200}
+                            className="mx-auto max-h-48 w-full flex-shrink-0 rounded-t-lg pb-2"
+                            src={imageURL}
+                            alt={imageAlt}
+                        />
+                        <button
+                            onClick={toggleAudio}
+                            type="button"
+                            className="absolute right-5 left-0 top-[15%] m-auto w-9 rounded-full p-2 text-white shadow-sm"
+                        >
+                            {!play ? (
+                                <PlayIcon className="h-12 w-12" aria-hidden="true" />
+                            ) : (
+                                <PauseIcon className="h-12 w-12" aria-hidden="true" />
+                            )}
+                        </button>
+                        <dl className="mt-1 flex flex-col p-4 ">
+                            <dd className="text-lg text-white">{soundTitle}</dd>
+                        </dl>
+                        <div className="mx-4 flex">
+                            <input
+                                type="range"
+                                className="mr-2 w-full accent-cyan-700"
+                                min={0}
+                                max={MAX}
+                                onChange={(e) => handleVolume(e)}
+                            />
+                            <SpeakerWaveIcon
+                                className="h-5 w-5 text-white"
+                                aria-hidden="true"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <audio ref={pokeRef} src={audioSrc} />
+            </main>
+        </>
+    )
+}
--- app/ui/pokefinder/DataDisplay.jsx
+++ app/ui/pokefinder/DataDisplay.jsx
@@ -120,7 +120,9 @@ export default function DataDisplay() {
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <dt className="text-sm font-medium leading-6 text-white">{`${selectedPokemon}'s Latest Cry`}</dt>
                             <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">
-                                <audio src={latestCry} controls ></audio>
+                                <audio src={latestCry}>
+                                    It looks like your browser doesn't support this audio player.
+                                </audio>
                             </dd>
                         </div>
 
