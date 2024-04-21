c8ed0c0718a5291f92933548ea9ca6c63e9bdc5b
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-21 15:20:10 -0400

    updated to 4 cols per row

--- app/ui/wiredex/AllMyPokemon.jsx
+++ app/ui/wiredex/AllMyPokemon.jsx
@@ -57,7 +57,7 @@ export default function AllMyPokemon() {
                     }
                 </div>
 
-                <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+                <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-4 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
                     {
                         status === "loading"
                             ?
@@ -67,36 +67,36 @@ export default function AllMyPokemon() {
                                 {/* // ! Map all pokemon */}
                                 {
                                     allPokemonNames.length > 0
-                                    ?
+                                        ?
 
-                                    allPokemonNames.map((name, index) => (
-                                        <div className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2">
+                                        allPokemonNames.map((name, index) => (
+                                            <div className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2">
 
-                                    <img
-                                        src={allPokemonPictures[index]}
-                                        alt={name}
-                                        className="object-cover object-center group-hover:opacity-75"
-                                    />
+                                                <img
+                                                    src={allPokemonPictures[index]}
+                                                    alt={name}
+                                                    className="object-cover object-center group-hover:opacity-75"
+                                                />
 
-                                    <div aria-hidden="true" className="bg-gradient-to-b from-transparent to-black opacity-50" />
-                                    <div className="flex items-end p-6">
-                                        <div>
-                                            <h3 className="font-semibold text-white">
-                                                <a href="#">
-                                                    <span className="absolute inset-0" />
-                                                            {name}
-                                                </a>
-                                            </h3>
-                                            <p aria-hidden="true" className="mt-1 text-sm text-white">
-                                                View Stats
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                    ))
-                                    :
+                                                <div aria-hidden="true" className="bg-gradient-to-b from-transparent to-black opacity-50" />
+                                                <div className="flex items-end p-6">
+                                                    <div>
+                                                        <h3 className="font-semibold text-white">
+                                                            <a href="#">
+                                                                <span className="absolute inset-0" />
+                                                                {name}
+                                                            </a>
+                                                        </h3>
+                                                        <p aria-hidden="true" className="mt-1 text-sm text-white">
+                                                            View Stats
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))
+                                        :
 
-                                    null
+                                        null
                                 }
                             </>
                     }
