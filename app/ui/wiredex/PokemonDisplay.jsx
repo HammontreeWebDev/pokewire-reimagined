01ace35d8d8813202863b819b8d5b843f3952b06
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-20 23:16:12 -0400

    successfully displaying first 3 pokemon

--- app/ui/wiredex/PokemonDisplay.jsx
+++ app/ui/wiredex/PokemonDisplay.jsx
@@ -24,14 +24,18 @@ export default function PokemonDisplay() {
                     throw new Error('failed to fetch pokemons');
                 }
                 const data = await response.json();
-                console.log('All Data: ', data)
-                console.log('Debug Data: ', data[0].sprites[0].main)
 
-                // * Set States for display measures:
-                setFirstPokemonName(data[0].name);
-                setFirstPokemonPicture(data[0].sprites[0].main);
-                setAllPokemonNames(data.map((name) => name.name))
-                setAllPokemonPictures(data.map((picture) => picture.sprites[0].main));
+                if (data.length > 0) {
+
+                    // * Set first pokemon:
+                    setFirstPokemonName(data[0].name);
+                    setFirstPokemonPicture(data[0].sprites[0].main);
+
+                    // * Set all pokemon and exclude the first:
+                    const remainingPokemon = data.slice(1);
+                    setAllPokemonNames(remainingPokemon.map((name) => name.name))
+                    setAllPokemonPictures(remainingPokemon.map((picture) => picture.sprites[0].main));
+                }
 
             } catch (error) {
                 console.error('Error fetching pokemons:', error);
@@ -40,10 +44,6 @@ export default function PokemonDisplay() {
 
         fetchPokemons();
     }, [status]);
-
-    useEffect(() => {
-        console.log('State Data: ', allPokemonPictures)
-    }, [allPokemonPictures])
 
     return (
         <div className="bg-poke-black w-full">
@@ -61,73 +61,84 @@ export default function PokemonDisplay() {
                 </div>
 
                 <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
-
-                    {/* // ! large picture */}
-                    <div className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2">
-                        <img
-                            src={
-                                firstPokemonPicture !== null
-                                    ?
-                                    firstPokemonPicture
-                                    :
-                                    "https://tailwindui.com/img/ecommerce-images/home-page-03-featured-category.jpg"
-                            }
-                            alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee."
-                            className="object-cover object-center group-hover:opacity-75"
-                        />
-                        <div aria-hidden="true" className="bg-gradient-to-b from-transparent to-black opacity-50" />
-                        <div className="flex items-end p-6">
-                            <div>
-                                <h3 className="font-semibold text-white">
-                                    <a href="#">
-                                        <span className="absolute inset-0" />
-                                        {
-                                            firstPokemonName !== null ?
-                                                firstPokemonName
-                                                :
-                                                'Add your pokémon in the pokéfinder!'
-                                        }
-                                    </a>
-                                </h3>
-                                <p aria-hidden="true" className="mt-1 text-sm text-white">
-                                    View Stats
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* // ! small picture */}
                     {
-                        allPokemonNames.map((name, index) => (
-                            <div 
-                            className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full"
-                            key={index}
-                            >
-                                <img
-                                // The src needs to map allPokemonPictures here. . . the order in each array already coresponds to each data type
-                                    src={allPokemonPictures[index]}
-                                    alt="Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters."
-                                    className="object-cover object-center group-hover:opacity-75 sm:absolute sm:inset-0 sm:h-full sm:w-full"
-                                />
-                                <div
-                                    aria-hidden="true"
-                                    className="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"
-                                />
-                                <div className="flex items-end p-6 sm:absolute sm:inset-0">
-                                    <div>
-                                        <h3 className="font-semibold text-white">
-                                            <a href="#">
-                                                <span className="absolute inset-0" />
-                                                {name}
-                                            </a>
-                                        </h3>
-                                        <p aria-hidden="true" className="mt-1 text-sm text-white">
-                                            View Stats
-                                        </p>
+                        status === "loading"
+                            ?
+                            <p className="text-poke-white anitmate-pulse"> . . . Loading</p>
+                            :
+                            <>
+                                {/* // ! large picture */}
+                                <div className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-h-1 sm:aspect-w-1 sm:row-span-2">
+                                    {
+                                        firstPokemonPicture !== null
+                                            ?
+                                            <img
+                                                src={firstPokemonPicture}
+                                                alt={firstPokemonName}
+                                                className="object-cover object-center group-hover:opacity-75"
+                                            />
+                                            :
+                                            null
+                                    }
+                                    <div aria-hidden="true" className="bg-gradient-to-b from-transparent to-black opacity-50" />
+                                    <div className="flex items-end p-6">
+                                        <div>
+                                            <h3 className="font-semibold text-white">
+                                                <a href="#">
+                                                    <span className="absolute inset-0" />
+                                                    {
+                                                        firstPokemonName !== null ?
+                                                            firstPokemonName
+                                                            :
+                                                            'Add your pokémon in the pokéfinder!'
+                                                    }
+                                                </a>
+                                            </h3>
+                                            <p aria-hidden="true" className="mt-1 text-sm text-white">
+                                                View Stats
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
+
+                                {/* // ! small picture */}
+                                {
+                                    allPokemonNames.length > 0
+                                        ?
+                                        allPokemonNames.map((name, index) => (
+                                            <div
+                                                className="group aspect-h-1 aspect-w-2 overflow-hidden rounded-lg sm:aspect-none sm:relative sm:h-full"
+                                                key={index}
+                                            >
+                                                <img
+                                                    // The src needs to map allPokemonPictures here. . . the order in each array already coresponds to each data type
+                                                    src={allPokemonPictures[index]}
+                                                    alt={name}
+                                                    className="object-cover object-center group-hover:opacity-75 sm:absolute sm:inset-0 sm:h-full sm:w-full"
+                                                />
+                                                <div
+                                                    aria-hidden="true"
+                                                    className="bg-gradient-to-b from-transparent to-black opacity-50 sm:absolute sm:inset-0"
+                                                />
+                                                <div className="flex items-end p-6 sm:absolute sm:inset-0">
+                                                    <div>
+                                                        <h3 className="font-semibold text-white">
+                                                            <a href="#">
+                                                                <span className="absolute inset-0" />
+                                                                {name}
+                                                            </a>
+                                                        </h3>
+                                                        <p aria-hidden="true" className="mt-1 text-sm text-white">
+                                                            View Stats
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))
+                                        :
+                                        null
+                                }
+                            </>
                     }
                 </div>
 
