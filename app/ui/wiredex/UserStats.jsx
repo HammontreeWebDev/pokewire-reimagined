dc009df96fe7aa642c468d26c6346e14871dedb7
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-04-22 16:26:21 -0400

    updated stats title to be able to toggle

--- app/ui/wiredex/UserStats.jsx
+++ app/ui/wiredex/UserStats.jsx
@@ -8,6 +8,7 @@ export default function UserStats() {
     const [numberOfPokemon, setNumberOfPokemon] = useState(0);
     const { saveState } = useSave();
     const [userName, setUserName] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         if (status === 'authenticated') {
@@ -25,12 +26,12 @@ export default function UserStats() {
                     }
                     const data = await response.json();
                     setPokemons(data);
-    
+
                 } catch (error) {
                     console.error('Error fetching pokemons:', error);
                 }
             };
-    
+
             fetchPokemons();
         }
     }, [status, saveState]);
@@ -46,46 +47,83 @@ export default function UserStats() {
         }
     }, [status === 'authenticated']);
 
+
+    function handleViewStats() {
+        setIsOpen(!isOpen);
+    }
+
+
     return (
-        <div className="w-full flex justify-center border-b bg-gradient-to-br from-transparent to-[var(--poke-yellow)]">
-            <div className="my-3 py-3 flex justify-center antialiased rounded w-1/2">
+        <>
 
-                <div className="flex flex-col justify-center mx-3 bg-trans-black p-3 rounded">
-                    {/* // ! Pokémon Count */}
+            {
+                isOpen
+                    ?
+                    <>
+                        <div className="w-full flex justify-center items-center bg-poke-black font-bold">
+                            <button
+                                className="text-poke-white hover:animate-pulse"
+                                onClick={handleViewStats}
+                            >
+                                Close
+                            </button>
+                        </div>
+                        <div className="w-full flex justify-center border-b bg-gradient-to-br from-transparent to-[var(--poke-yellow)]">
+                            <div className="flex justify-center antialiased">
 
-                    {
-                        status === 'loading'
-                            ?
-                            <div className="animate-pulse text-poke-red">. . . Loading</div>
-                            :
-                            <h1 className="text-poke-white text-center border-b font-extrabold uppercase">
-                                {userName}'s
-                                <span className="text-poke-red">
-                                    &nbsp;Stats
-                                </span>
-                            </h1>
-                    }
+                                <div className="flex flex-col justify-center bg-trans-black p-3 w-screen">
+                                    {/* // ! Pokémon Count */}
 
-                    <p className="text-poke-yellow font-bold">Pokémon Count:
-                        <span className="ml-1 px-1 rounded text-poke-white">
-                            {
-                                status === 'loading'
-                                    ?
-                                    '. . . Loading'
-                                    :
-                                    numberOfPokemon
-                            }
-                        </span>
-                    </p>
+                                    {
+                                        status === 'loading'
+                                            ?
+                                            <div className="animate-pulse text-poke-red">. . . Loading</div>
+                                            :
+                                            <h1 className="text-poke-white text-center border-b font-extrabold uppercase">
+                                                {userName}'s
+                                                <span className="text-poke-red">
+                                                    &nbsp;Stats
+                                                </span>
+                                            </h1>
+                                    }
 
-                    {/* //! Favorite Pokémon */}
-                    <p className="text-poke-yellow font-bold">Favorite Pokémon:
-                        <span className="ml-1 px-1 rounded text-poke-white">
-                            Placeholder Pikachu
-                        </span>
-                    </p>
-                </div>
-            </div>
-        </div>
+                                    <p className="text-poke-yellow font-bold self-center">Pokémon Count:
+                                        <span className="ml-1 px-1 rounded text-poke-white">
+                                            {
+                                                status === 'loading'
+                                                    ?
+                                                    '. . . Loading'
+                                                    :
+                                                    numberOfPokemon
+                                            }
+                                        </span>
+                                    </p>
+
+                                    {/* //! Favorite Pokémon */}
+                                    <p className="text-poke-yellow font-bold self-center">Favorite Pokémon:
+                                        <span className="ml-1 px-1 rounded text-poke-white">
+                                            Placeholder Pikachu
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                    :
+
+                    <div className="w-full flex justify-center items-center bg-poke-black font-bold">
+                        <button
+                            className="text-poke-white hover:animate-pulse"
+                            onClick={handleViewStats}
+                        >
+                            View My
+                            <span className="text-poke-red">
+                                &nbsp;Stats
+                            </span>
+                        </button>
+                    </div>
+            }
+
+        </>
     )
 }
