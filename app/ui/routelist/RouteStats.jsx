f6d7b40e90eadba69a53f875f3dd7f02ec780c3b
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-06-06 16:43:27 -0400

    cleaned up code to avoid errors

--- app/ui/routelist/RouteStats.jsx
+++ app/ui/routelist/RouteStats.jsx
@@ -16,11 +16,11 @@ export default function RouteStats({ selectedPokemon }) {
     <div className="bg-gray-900">
       <div className="mx-auto max-w-7xl">
         <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
-          {routeData.map((route, index) => (
+          {routeData?.map((route, index) => (
             <div key={index} className="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
               <p className="text-sm font-medium leading-6 text-gray-400">{`Location: ${route.location_area.name}`}</p>
               <div className="mt-2">
-                {route.version_details.map((version, vIndex) => (
+                {route.version_details?.map((version, vIndex) => (
                   <div key={vIndex} className="mb-4">
                     <p className="text-sm font-medium leading-6 text-gray-400">{`Version: ${version.version.name}`}</p>
                     {version.encounter_details.map((detail, dIndex) => (
