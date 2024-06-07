405489eb1a9caed70bf5c9ec4999245b53f7462a
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-06-07 14:34:23 -0400

    refactored to use only limited data

--- app/ui/routelist/RouteStats.jsx
+++ app/ui/routelist/RouteStats.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
+import { ArrowsPointingOut, XMarkIcon } from "../svg/svgImagesHelper";
 
 export default function RouteStats({ selectedPokemon }) {
-
   const [routeData, setRouteData] = useState([]);
+  const [expandedSections, setExpandedSections] = useState({});
 
   useEffect(() => {
     if (selectedPokemon && selectedPokemon.routes) {
@@ -12,31 +13,62 @@ export default function RouteStats({ selectedPokemon }) {
     }
   }, [selectedPokemon]);
 
+  useEffect(() => {
+    console.log("DATA: ", routeData);
+  }, [routeData]);
+
+  const toggleSection = (index) => {
+    setExpandedSections((prev) => ({
+      ...prev,
+      [index]: !prev[index]
+    }));
+  };
+
+  useEffect(() => { console.log("Expanded Section: ", expandedSections) }, [expandedSections]);
+
   return (
-    <div className="bg-gray-900">
-      <div className="mx-auto max-w-7xl">
-        <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
+    <div className="bg-gray-900 min-h-screen overflow-y-auto">
+      <div className="mx-auto max-w-7xl p-4">
+        <div className="flex flex-col gap-4">
           {routeData?.map((route, index) => (
-            <div key={index} className="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-              <p className="text-sm font-medium leading-6 text-gray-400">{`Location: ${route.location_area.name}`}</p>
-              <div className="mt-2">
-                {route.version_details?.map((version, vIndex) => (
-                  <div key={vIndex} className="mb-4">
-                    <p className="text-sm font-medium leading-6 text-gray-400">{`Version: ${version.version.name}`}</p>
-                    {version.encounter_details.map((detail, dIndex) => (
-                      <div key={dIndex} className="mt-2 flex items-baseline gap-x-2">
-                        <p className="text-sm text-gray-400">{`Method: ${detail.method.name}`}</p>
-                        <p className="text-sm text-gray-400">{`Chance: ${detail.chance}`}</p>
-                        <p className="text-sm text-gray-400">{`Min Level: ${detail.min_level}`}</p>
-                        <p className="text-sm text-gray-400">{`Max Level: ${detail.max_level}`}</p>
-                        {detail.condition_values.length > 0 && (
-                          <p className="text-sm text-gray-400">{`Condition: ${detail.condition_values.map(cond => cond.name).join(', ')}`}</p>
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                ))}
+            <div key={index} className="bg-poke-black my-2 px-4 py-4 rounded-2xl">
+              <div className="bg-poke-black px-4 py-4">
+                <button
+                  className="text-poke-white"
+                  onClick={() => toggleSection(index)}>
+                  <ArrowsPointingOut />
+                </button>
+                <h2 className="text-2xl text-poke-red text-center font-bold">Location:</h2>
+                <p className="text-lg font-medium leading-6 text-poke-white text-center">
+                  {route.location_area.name
+                    .split('-')
+                    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                    .join(' ')}
+                </p>
               </div>
+              {expandedSections[index] && (
+                <div className="bg-poke-black mt-2 rounded-2xl">
+                  {route.version_details?.map((version, vIndex) => (
+                    <div key={vIndex} className="bg-poke-black px-4 py-4 mt-2 border-t border-[var(--poke-white)]">
+
+                      <p className="text-lg text-poke-blue text-left font-bold">Version:&nbsp;
+                        <span className="text-md font-medium text-poke-white text-left">
+                          {version.version.name
+                            .split('-')
+                            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                            .join(' ')}
+                        </span>
+                      </p>
+
+                      <p className="text-lg text-poke-yellow text-left font-bold">Max Chance Of Encounter:&nbsp;
+                        <span className="text-md font-medium text-poke-white text-left">
+                          {`${version.max_chance}%`}
+                        </span>
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           ))}
         </div>
